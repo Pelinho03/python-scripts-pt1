d45926d9e579fe907d109a53f7d7b83ab244143c
Paulo Guimarães <elmuchacho@outlook.pt>
2024-09-10 19:12:32 +0100

    update ex021.py

--- scripts/PythonExercises/ex021.py
+++ scripts/PythonExercises/ex021.py
@@ -1,5 +1,12 @@
+'''
+021. Fazer um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+'''
 import pygame
-pygame.init()
-pygame.mixer.music.load('feelings.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+
+pygame.mixer.init()  # Inicializa o mixer
+pygame.mixer.music.load('feelings.mp3')  # Carrega o arquivo MP3
+pygame.mixer.music.play()  # Reproduz o arquivo MP3
+
+# Mantém o script a correr até que o áudio termine
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)  # Aguarda até que a reprodução termine
